2019afc88d36c73352d2a5e74be00ec6daf59431
Viktoar <57002818+Viktoar@users.noreply.github.com>
2021-03-04 18:19:12 +0300

    'nice'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 $(function () {
 
+    $('.top__slider').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        fade: true,
+    });
+
+    $('.feedback__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+        variableWidth: true,
+    });
+
+
+
+    $('.tabs-All').on("click", function () {
+        elem = document.getElementById("tabs_container").className = "tabs__body-All";
+    });
+    $('.tabs-Healthcare').on("click", function () {
+        elem = document.getElementById("tabs_container").className = "tabs__body-Healthcare";
+    });
+    $('.tabs-Green').on("click", function () {
+        elem = document.getElementById("tabs_container").className = "tabs__body-Green";
+    });
+    $('.tabs-Interior').on("click", function () {
+        elem = document.getElementById("tabs_container").className = "tabs__body-Interior";
+    });
+    $('.tabs-Office').on("click", function () {
+        elem = document.getElementById("tabs_container").className = "tabs__body-Office";
+    });
 })
