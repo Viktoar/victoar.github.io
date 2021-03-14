a68ee64c9bd706fbac63db4f7571ab103bbe1594
Viktoar <57002818+Viktoar@users.noreply.github.com>
2021-03-14 20:01:55 +0300

    'x'

--- js/main.js
+++ js/main.js
@@ -16,24 +16,34 @@ $(function () {
         autoplay: true,
         arrows: false,
         dots: true,
-
+        variableWidth: false,
     });
 
 
 
     $('.tabs-All').on("click", function () {
+        $('.tabs__item').css({ 'color': 'black' });
+        $('.tabs-All').css({ 'color': '#fab702' });
         elem = document.getElementById("tabs_container").className = "tabs__body-All";
     });
     $('.tabs-Healthcare').on("click", function () {
+        $('.tabs__item').css({ 'color': 'black' });
+        $('.tabs-Healthcare').css({ 'color': '#fab702' });
         elem = document.getElementById("tabs_container").className = "tabs__body-Healthcare";
     });
     $('.tabs-Green').on("click", function () {
+        $('.tabs__item').css({ 'color': 'black' });
+        $('.tabs-Green').css({ 'color': '#fab702' });
         elem = document.getElementById("tabs_container").className = "tabs__body-Green";
     });
     $('.tabs-Interior').on("click", function () {
+        $('.tabs__item').css({ 'color': 'black' });
+        $('.tabs-Interior').css({ 'color': '#fab702' });
         elem = document.getElementById("tabs_container").className = "tabs__body-Interior";
     });
     $('.tabs-Office').on("click", function () {
+        $('.tabs__item').css({ 'color': 'black' });
+        $('.tabs-Office').css({ 'color': '#fab702' });
         elem = document.getElementById("tabs_container").className = "tabs__body-Office";
     });
     $('.menu__btn').on('click', function () {
