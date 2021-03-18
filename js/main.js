31d78f76ce2641b52befb3dc55318c26794e17f0
Viktoar <57002818+Viktoar@users.noreply.github.com>
2021-03-18 12:19:54 +0300

    'v'

--- js/main.js
+++ js/main.js
@@ -29,6 +29,24 @@ $(function () {
         ]
     });
 
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function () {
+                if ($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+
 
 
     $('.tabs-All').on("click", function () {
